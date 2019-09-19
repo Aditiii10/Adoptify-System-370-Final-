393e3cdf6398b61da1e806865a4c552a5636e508
Divin <divinlonji@gmail.com>
2019-09-20 01:16:50 +0200

    Created and successfully implemented Custom Email Sender

---
+++ Adoptify-System-370-Final-/AdoptifySystem/AdoptifySystem/Controllers/EmailController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdoptifySystem;
+
+namespace AdoptifySystem.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Index()
+        {
+            //TempData["EmailSucessMessage"] = "Email Successfully Sent";
+            //TempData["EmailErrorMessage"] = "Email Unseccessfully Sent";
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult SendEmailView()
+        {
+            //call SendEmailView view to invoke webmail  
+            return View();
+        }
+    }
+}
